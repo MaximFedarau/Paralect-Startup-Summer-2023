d90a73c52bfd6c414911df4ce7c4a6a2f1124ba0
maxim <fedaraumaxim@gmail.com>
2023-05-23 11:41:02 +0300

    feat: install & setup redux-persist

--- apps/web/store/index.ts
+++ apps/web/store/index.ts
@@ -1,10 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  PersistConfig,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 import { rootReducer } from "./rootReducer";
 
+const persistConfig: PersistConfig<unknown, any, any, any> = {
+  key: "root",
+  storage,
+  blacklist: ["vacanciesForm"],
+};
+
+const persistedRootReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedRootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
